c0e0d4ed94ba6d1d614e1a315bd0bfadfddbc6e9
Nicole <npinilla89@gmail.com>
2024-10-21 21:48:42 -0300

    se agregan elementos de boostrap

--- listaEmpleados/lista/views.py
+++ listaEmpleados/lista/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def listaEmpleados(request):
- listado = ['Aurora Jimenez', 'Mateo Diaz', 'Juan Perez', 'Nicolas Clos', 'Felipe Vergara', 'Maria Diaz', 'Veronica Soto', 'Benito Juares', 'benjamin Duarte']
+ listado = ['Aurora Jimenez', 'Mateo Diaz', 'Juan Perez', 'Nicolas Clos', 'Felipe Vergara', 'Maria Diaz', 'Veronica Soto', 'Benito Juares', 'Benjamin Duarte']
  context = {'listado': listado}
  return render(request, 'listaEmpleados.html ', context)
